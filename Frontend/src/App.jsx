a6dab9f210244330b9f88a1260b889b1e7547321
Fabio <fabioormeno90torres@gmail.com>
2026-05-27 22:36:52 -0500

    Actualizada la URL del backend apuntando a Render

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
     try {
       // Enviamos los datos al Backend de Flask
-      const response = await fetch("http://127.0.0.1:5000/api/login", {
+      const response = await fetch("https://proyecto-parcial-fullstack.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
